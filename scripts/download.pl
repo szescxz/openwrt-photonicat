fc59ab3c087773ba281b472bf7c407d7e5ff63e6
Marty Jones <mj8263788@gmail.com>
2023-09-29 14:08:59 -0400

    kernel: update to linux-next v6.7-rc20230929

--- scripts/download.pl
+++ scripts/download.pl
@@ -315,7 +315,7 @@ foreach my $mirror (@ARGV) {
 	}
 }
 
-push @mirrors, 'https://github.com/mj22226/linux/archive/refs/tags/v6.7-rc20230927/';
+push @mirrors, 'https://github.com/mj22226/linux/archive/refs/tags/v6.7-rc20230929/';
 push @mirrors, 'https://sources.cdn.openwrt.org';
 push @mirrors, 'https://sources.openwrt.org';
 push @mirrors, 'https://mirror2.openwrt.org/sources';
