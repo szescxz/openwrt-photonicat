a1552b2cb48588996aaeef5f0e8e25779ba4bf2d
szescxz <49463229+szescxz@users.noreply.github.com>
2023-11-14 09:11:12 +0800

    uboot: rockchip: fix photonicat-rk3568

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -166,6 +166,6 @@ define Device/ariaboard_photonicat
   SOC := rk3568
   UBOOT_DEVICE_NAME := photonicat-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script photonicat-rk3568 | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := ath10k-firmware-qca9377-sdio kmod-ath10k kmod-ath10k-sdio pcat-manager
+  DEVICE_PACKAGES := ath10k-firmware-qca9377-sdio kmod-ath10k kmod-ath10k-sdio
 endef
 TARGET_DEVICES += ariaboard_photonicat
